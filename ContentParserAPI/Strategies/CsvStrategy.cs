ebe4f6ae1a6d15af694ecf66e27a219fec89fcd3
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-07-30 13:03:56 +0200

    WIP: implement InternalJsonStrategy

---
+++ ContentParserAPI/Strategies/CsvStrategy.cs
@@ -0,0 +1,8 @@
+﻿using ContentParserAPI.DTOs;
+using ContentParserAPI.Interfaces;
+
+namespace ContentParserAPI.Strategies;
+
+public class CsvStrategy : IContentParser
+{
+}
---
+++ ContentParserAPI/Strategies/InternalJsonStrategy.cs
@@ -0,0 +1,42 @@
+﻿using ContentParserAPI.DTOs;
+using ContentParserAPI.Interfaces;
+using System.Text.Json;
+
+namespace ContentParserAPI.Strategies;
+
+public class InternalJsonStrategy : IContentParser
+{
+    public Task<ParseResponse> ParseData(string data)
+    {
+        try
+        {
+            var deserializedData = JsonSerializer.Deserialize<IEnumerable<Dictionary<string, object>>>(data); // TODO refactor to use stream and async
+
+            if (deserializedData is not null)
+            {
+                return Task.FromResult(new ParseResponse
+                {
+                    Status = true,
+                    ProcessedCount = deserializedData.Count(),
+                    Data = deserializedData
+                });
+            }
+
+            return Task.FromResult(new ParseResponse
+            {
+                Status = false,
+                ProcessedCount = 0,
+                Data = []
+            });
+        }
+        catch (Exception)
+        {
+            return Task.FromResult(new ParseResponse
+            {
+                Status = false,
+                ProcessedCount = 0,
+                Data = []
+            });
+        }
+    }
+}

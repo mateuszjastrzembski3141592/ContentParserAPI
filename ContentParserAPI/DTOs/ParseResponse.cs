05652874b629cb40739efd59e9dcd855a74c3100
MateuszJastrzembski <92751694+mateuszjastrzembski3141592@users.noreply.github.com>
2026-07-31 16:21:30 +0200

    feat: implement ContentProcessor service

--- ContentParserAPI/DTOs/ParseResponse.cs
+++ ContentParserAPI/DTOs/ParseResponse.cs
@@ -5,4 +5,11 @@ public class ParseResponse
     public required bool Status { get; init; }
     public required int ProcessedCount { get; init; }
     public required IEnumerable<Dictionary<string, object>> Data { get; init; }
+
+    public static ParseResponse DefaultResponse { get; } = new()
+    {
+        Status = false,
+        ProcessedCount = 0,
+        Data = []
+    };
 }
---
+++ ContentParserAPI/Interfaces/IContentProcessor.cs
@@ -0,0 +1,8 @@
+﻿using ContentParserAPI.DTOs;
+
+namespace ContentParserAPI.Interfaces;
+
+public interface IContentProcessor
+{
+    Task<ParseResponse?> ProcessContentAsync(ParseRequest requestContent);
+}
